47b80fd5799cac563052b96523f5bc8db7b20267
Antony <meiyixuan2000@126.com>
2021-12-09 17:22:59 +0800

    finished 3.3 Pixel.h

--- src/Pixel.h
+++ src/Pixel.h
@@ -9,13 +9,20 @@
 
 class Pixel {
 public:
-    Pixel() : pixel_color() {}
+    Pixel() : pixel_color(), write_flag(false) {}
 
     void set(const Color &color) { pixel_color = color; }
 
     Color get() { return pixel_color; }
 
     void write() {
+        // a pixel can only be written once for correctness
+        if (!write_flag) {
+            write_flag = true;
+        } else {
+            throw std::runtime_error("A pixel is written twice");
+        }
+
         // scale to [0, 255]
         std::cout << static_cast<int>(255.999 * pixel_color.x()) << ' '
                   << static_cast<int>(255.999 * pixel_color.y()) << ' '
@@ -23,6 +30,7 @@ public:
     }
 
 private:
+    bool write_flag;
     Color pixel_color;
 };
 
