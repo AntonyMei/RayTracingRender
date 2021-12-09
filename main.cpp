acc3780378755b830cb141e7bfe46b001dadfa89
Antony <meiyixuan2000@126.com>
2021-12-09 19:10:52 +0800

    finished 4

--- main.cpp
+++ main.cpp
@@ -7,16 +7,33 @@
 #include "src/Pixel.h"
 #include "src/Ray.h"
 
+Color ray_color(const Ray& r) {
+    Vector3d unit_direction = normalize(r.direction());
+    auto t = 0.5*(unit_direction.y() + 1.0);
+    return (1.0-t)*Color(1.0, 1.0, 1.0) + t*Color(0.5, 0.7, 1.0);
+}
+
 int main() {
 
     // Image
-    const int image_width = 256;
-    const int image_height = 256;
+    const auto aspect_ratio = 16.0 / 9.0;
+    const int image_width = 400;
+    const int image_height = static_cast<int>(image_width / aspect_ratio);
     auto image = std::vector<std::vector<Pixel>>();
     for (int idx = 0; idx < image_height; ++idx) {
         auto row = std::vector<Pixel>(image_width);
         image.push_back(std::move(row));
     }
+
+    // Camera
+    auto viewport_height = 2.0;
+    auto viewport_width = aspect_ratio * viewport_height;
+    auto focal_length = 1.0;
+
+    auto origin = Point3(0, 0, 0);
+    auto horizontal = Vector3d(viewport_width, 0, 0);
+    auto vertical = Vector3d(0, viewport_height, 0);
+    auto lower_left_corner = origin - horizontal/2 - vertical/2 - Vector3d(0, 0, focal_length);
 
     // Render
     std::cout << "P3\n" << image_width << ' ' << image_height << "\n255\n";
@@ -24,10 +41,11 @@ int main() {
     for (int j = image_height-1; j >= 0; --j) {
         std::cerr << "\rScanlines remaining: " << j << ' ' << std::flush;
         for (int i = 0; i < image_width; ++i) {
-            auto r = double(i) / (image_width-1);
-            auto g = double(j) / (image_height-1);
-            auto b = 0.25;
-            image[j][i].set(Color(r, g, b));
+            auto u = double(i) / (image_width-1);
+            auto v = double(j) / (image_height-1);
+            Ray r(origin, lower_left_corner + u * horizontal + v * vertical - origin);
+            Color pixel_color = ray_color(r);
+            image[j][i].set(pixel_color);
             image[j][i].write();
         }
     }
