26d72b026131f20a78ef5ac8288333738edf8aba
Antony <meiyixuan2000@126.com>
2021-12-11 22:00:55 +0800

    branched from multi-threading

--- main.cpp
+++ main.cpp
@@ -39,8 +39,8 @@ int main() {
     const auto aspect_ratio = 16.0 / 9.0;
     const int image_width = 800;
     const int image_height = static_cast<int>(image_width / aspect_ratio);
-    const int samples_per_pixel = 100;
-    const int max_depth = 50;
+    const int samples_per_pixel = 30;
+    const int max_depth = 10;
     auto image = std::vector<std::vector<Pixel>>();
     for (int idx = 0; idx < image_height; ++idx) {
         auto row = std::vector<Pixel>(image_width);
