b4cbad720c3c7533c310769bf136b96237d81ea0
Antony <meiyixuan2000@126.com>
2021-12-13 20:00:17 +0800

    finished chapter 10, add dielectrics

--- main.cpp
+++ main.cpp
@@ -66,6 +66,7 @@ int main() {
     world.add(make_shared<Sphere>(Point( 0.0, -100.5, -1.0), 100.0, material_ground));
     world.add(make_shared<Sphere>(Point( 0.0,    0.0, -1.0),   0.5, material_center));
     world.add(make_shared<Sphere>(Point(-1.0,    0.0, -1.0),   0.5, material_left));
+    world.add(make_shared<Sphere>(Point(-1.0,    0.0, -1.0),  -0.4, material_left));
     world.add(make_shared<Sphere>(Point( 1.0,    0.0, -1.0),   0.5, material_right));
 
     // Camera
