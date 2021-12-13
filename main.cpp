da2d3f034f1851808a6dd7490ae0154c9a727f07
Antony <meiyixuan2000@126.com>
2021-12-13 16:20:04 +0800

    fixed a bug in hit (one path won't assign value to hit.mat_ptr)

--- main.cpp
+++ main.cpp
@@ -48,7 +48,7 @@ int main() {
     const auto aspect_ratio = 16.0 / 9.0;
     const int image_width = 1024;
     const int image_height = static_cast<int>(image_width / aspect_ratio);
-    const int samples_per_pixel = 1;
+    const int samples_per_pixel = 100;
     const int max_depth = 50;
     auto image = std::vector<std::vector<Pixel>>();
     for (int idx = 0; idx < image_height; ++idx) {
