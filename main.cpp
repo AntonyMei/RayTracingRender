8434a47e057c4829ec54872c3185e80c8ae9607e
Antony <meiyixuan2000@126.com>
2021-12-13 21:32:45 +0800

    modified scene

--- main.cpp
+++ main.cpp
@@ -231,6 +231,69 @@ void render_hollow_glass_ball_off_focus() {
         std::cerr << "\nDone.\n";
 };
 
+void render_many_balls() {
+    // Image
+    const auto aspect_ratio = 16.0 / 9.0;
+    const int image_width = 400; // 3840, 800
+    const int image_height = static_cast<int>(image_width / aspect_ratio);
+    const int samples_per_pixel = 10; // 1000, 100
+    const int max_depth = 25; // 100, 50
+    auto image = std::vector<std::vector<Pixel>>();
+    for (int idx = 0; idx < image_height; ++idx) {
+        auto row = std::vector<Pixel>(image_width);
+        image.push_back(std::move(row));
+    }
+
+    // World
+    HittableList world;
+
+    auto material_ground = make_shared<Lambertian>(Color(0.8, 0.8, 0.0));
+    auto material_center = make_shared<Lambertian>(Color(0.1, 0.2, 0.5));
+    auto material_left = make_shared<Dielectric>(1.5);
+    auto material_right = make_shared<Metal>(Color(0.8, 0.6, 0.2), 0.0);
+
+    world.add(make_shared<Sphere>(Point(0.0, -100.5, -1.0), 100.0, material_ground));
+    world.add(make_shared<Sphere>(Point(0.0, 0.0, -1.0), 0.5, material_center));
+    world.add(make_shared<Sphere>(Point(-1.0, 0.0, -1.0), 0.5, material_left));
+    world.add(make_shared<Sphere>(Point(-1.0, 0.0, -1.0), -0.45, material_left));
+    world.add(make_shared<Sphere>(Point(1.0, 0.0, -1.0), 0.5, material_right));
+
+    // Camera
+    Point camera_position(3, 3, 2);
+    Point view_point(0, 0, -1);
+    Vector3d camera_up(0, 1, 0);
+    auto dist_to_focus = (camera_position - view_point).length();
+    auto vertical_fov = 20;
+    auto aperture = 1.0;
+    SimpleCamera cam(camera_position, view_point, camera_up, vertical_fov, aspect_ratio,
+                     aperture, dist_to_focus);
+
+    // Render
+    std::cout << "P3\n" << image_width << ' ' << image_height << "\n255\n";
+    for (int j = image_height - 1; j >= 0; --j) {
+        std::cerr << "\rScanlines remaining: " << j << ' ' << std::flush;
+        for (int i = 0; i < image_width; ++i) {
+            Color pixel_color(0, 0, 0);
+            for (int s = 0; s < samples_per_pixel; ++s) {
+                auto u = (i + random_double()) / (image_width - 1);
+                auto v = (j + random_double()) / (image_height - 1);
+                Ray r = cam.get_ray(u, v);
+                pixel_color += cast_ray(r, world, max_depth);
+            }
+            image[j][i].set(pixel_color, samples_per_pixel);
+        }
+    }
+
+    // output
+    for (int j = image_height - 1; j >= 0; --j) {
+        for (int i = 0; i < image_width; ++i) {
+            image[j][i].write();
+        }
+    }
+
+    std::cerr << "\nDone.\n";
+}
+
 int main() {
-    render_hollow_glass_ball_small_fov();
+    render_many_balls();
 }
