09d25c6966e4323424f635f7e1c8ccb9646827cb
Antony <meiyixuan2000@126.com>
2021-12-15 16:35:43 +0800

    finished 3.5, add AABB

--- src/Accelerator.h
+++ src/Accelerator.h
@@ -24,6 +24,7 @@ public:
     virtual bool intersect(const Ray &r, double t_min, double t_max, Hit &rec) const = 0;
 };
 
+// a simple list
 class HittableList : public Accelerator {
 public:
     HittableList() = default;
@@ -52,6 +53,40 @@ public:
 
 private:
     std::vector<std::shared_ptr<Hittable>> hittable_list;
+};
+
+// bvh
+class AABB {
+public:
+    AABB() = delete;
+
+    AABB(const Point &min, const Point &max) : minimum(min), maximum(max) {}
+
+    Point min() const { return minimum; }
+
+    Point max() const { return maximum; }
+
+    inline bool hit(const Ray& ray, double t_min, double t_max) const {
+        // an optimized slab method (Andrew Kensler at Pixar)
+        for (int a = 0; a < 3; a++) {
+            auto invD = 1.0f / ray.direction()[a];
+            auto t0 = (min()[a] - ray.origin()[a]) * invD;
+            auto t1 = (max()[a] - ray.origin()[a]) * invD;
+            if (invD < 0.0f)
+                std::swap(t0, t1);
+            t_min = t0 > t_min ? t0 : t_min;
+            t_max = t1 < t_max ? t1 : t_max;
+            if (t_max <= t_min)
+                return false;
+        }
+        return true;
+    }
+
+
+private:
+    // ranges in three directions (i.e. two points in diagonal)
+    Point minimum;
+    Point maximum;
 };
 
 #endif //PROJECT_ACCELERATOR_H
