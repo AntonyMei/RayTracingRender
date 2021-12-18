a4abafdc3928efbfbc7ceaaebf6c547ba9a26f3e
Antony <meiyixuan2000@126.com>
2021-12-18 20:58:12 +0800

    finished 7.4

--- main.cpp
+++ main.cpp
@@ -258,7 +258,7 @@ void render_scene(int current_id, int max_processes, const char *output_file) {
     const auto aspect_ratio = 16.0 / 9.0;
     const int image_width = 400; // 3840, 800
     const int image_height = static_cast<int>(image_width / aspect_ratio);
-    const int samples_per_pixel = 100; // 1000, 100 (x4 if no global light)
+    const int samples_per_pixel = 400; // 1000, 100 (x4 if no global light)
     const int max_depth = 25; // 100, 50
 #else
     const auto aspect_ratio = 16.0 / 9.0;
