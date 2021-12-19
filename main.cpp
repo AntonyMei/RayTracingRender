c9e6d70f9c0faea06fb08c7873c2e334fb837012
Antony <meiyixuan2000@126.com>
2021-12-19 16:30:38 +0800

    finished chapter 10, add a complicated scene

--- main.cpp
+++ main.cpp
@@ -13,7 +13,7 @@ void render_scene(int current_id, int max_processes, const char *output_file) {
     const int max_depth = 10; // 100, 50
 #elif defined(DEBUG)
     const auto aspect_ratio = 1.0; // 16.0 / 9.0 or 1.0
-    const int image_width = 400; // 3840, 800
+    const int image_width = 1200; // 3840, 800
     const int image_height = static_cast<int>(image_width / aspect_ratio);
     const int samples_per_pixel = 400; // 1000, 100 (x4 if no global light)
     const int max_depth = 25; // 100, 50
