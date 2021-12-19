f5688609138f159a223bc39742dd01670363e09e
Antony <meiyixuan2000@126.com>
2021-12-19 23:00:00 +0800

    finished 6.1, book 3

--- src/core/Material.h
+++ src/core/Material.h
@@ -10,7 +10,14 @@ class Hit;
 class Material {
 public:
     virtual bool scatter(const Ray &ray_in, const Hit &hit, Color &attenuation,
-                         std::vector<Ray> &scattered_rays) const = 0;
+                         std::vector<Ray> &scattered_rays, double& pdf) const {
+        return false;
+    }
+
+    virtual double scattering_pdf(const Ray &ray_in, const Hit &hit,
+                                  std::vector<Ray> &scattered_rays) const {
+        return 0;
+    }
 
     virtual Color emit(double u, double v, const Point &p) const {
         return {0, 0, 0};
