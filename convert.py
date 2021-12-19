46fabd532921081d424d04ee51d70c7a91e0cb5e
Antony <meiyixuan2000@126.com>
2021-12-19 16:05:48 +0800

    finished chapter 10, add a complicated scene

--- convert.py
+++ convert.py
@@ -1,4 +1,4 @@
 import cv2
 
-i = cv2.imread('motion_blur_checker.ppm')
-cv2.imwrite('motion_blur_checker.jpg', i)
+i = cv2.imread('1.ppm')
+cv2.imwrite('1.jpg', i)
--- linux_run.sh
+++ linux_run.sh
@@ -1,7 +1,8 @@
-rm 1.ppm
 cmake .
 make clean
 make
 ./Project 60
+rm 1.ppm
+rm 1.jpg
 ./packager 60
 rm *.partial
