db4f0f522b4b178ba48149dd632cf141b544b708
Antony <meiyixuan2000@126.com>
2021-12-22 23:44:39 +0800

    finished object loading

--- src/scenes/Test.h
+++ src/scenes/Test.h
@@ -18,7 +18,7 @@ HittableList test_obj_scene() {
 
 SimpleCamera test_obj_camera(double aspect_ratio) {
     // basic settings
-    Point camera_position(-1.2, 1, 0);
+    Point camera_position(0, 1, 5);
     Point view_point(0, 1, 0);
     Vector3d camera_up(0, 1, 0);
     // fov
@@ -34,6 +34,6 @@ SimpleCamera test_obj_camera(double aspect_ratio) {
             aperture, dist_to_focus, shutter_open, shutter_close};
 }
 
-ConstantSkybox test_obj_skybox() { return ConstantSkybox({1, 1, 1}); }
+ConstantSkybox test_obj_skybox() { return ConstantSkybox({0, 0, 0}); }
 
 #endif //PROJECT_TEST_H
