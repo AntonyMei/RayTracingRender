4bf913fb36d665dd7f3d9e9a877e5d1fe77eed05
Antony <meiyixuan2000@126.com>
2021-12-23 17:44:10 +0800

    fixed a bug in SimpleSkybox.h

--- src/skybox/SimpleSkybox.h
+++ src/skybox/SimpleSkybox.h
@@ -14,7 +14,8 @@ public:
     Color get_color(Ray r) const override {
         Vector3d unit_direction = normalize(r.direction());
         auto t = 0.5 * (unit_direction.y() + 1.0);
-        return (1.0 - t) * Color(1.0, 1.0, 1.0) + t * Color(0.5, 0.7, 1.0) * intensity;
+        return ((1.0 - t) * Color(1.0, 1.0, 1.0) + t * Color(0.5, 0.7, 1.0))
+               * intensity;
     }
 
 private:
